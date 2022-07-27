c9fc7a33542b97703c64a4c647f08d22b3d87b02
ZakAKay <zakkay@ymail.com>
2022-07-27 14:46:13 -0500

    2.0

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
-//first require the dotenv config file
-require('dotenv').config()
-//this just checks to make sure secret api key is listed in the environment variables. uncomment to see. 
-console.log(process.env);
+// require("dotenv").config(); //standard for importing outside files or code
 
-//put the secret key in a variable
-const api_key = process.env.SECRET_API_KEY;
+// console.log(process.env); //
 
-console.log(api_key)
+// const api_key = process.env.SECRET_API_KEY;
+
+// console.log(api_key);
+
+let arrayOfCharacters = [];
+
+// This function waits for the web page to be loaded, when it does it will run the code inside of it which happens to be getPosts()
+window.onload = function () {
+  marvelHeroes();
+};
+
+// This function is going to make a fetch request to the URL inside its parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfCharacters
+const marvelHeroes = () => {
+  fetch(
+    "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d1c7a12f111e5223abc04519933d9cc2&hash=2a419df310c9ca8b004905f6f564a553&limit=10"
+  )
+    .then((res) => res.json())
+    .then((post) => (arrayOfCharacters = post.data.results))
+    .then(() => console.log("hellothere", arrayOfCharacters));
+};
+
+// This function logs the results in your browser's console
+// const consolePosts = () => {
+// }
+
+// this function creates elements inside the all-characters ul, then appends text inside it with the posts that were returned in the request.
+const displayPost = () => {
+  const allPosts = document.getElementById("all-characters");
+  arrayOfCharacters.map((post, index) => {
+    console.log("index", index, "post", post);
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = `${post.thumbnail.path}.${post.thumbnail.extension}`;
+    //  img.src = post.thumbnail.path + "." + post.thumbnail.extension   same as the line above
+    const text = document.createTextNode(`#${index}, Title: ${post.name}:`);
+    li.appendChild(text);
+    allPosts.append(li);
+    allPosts.append(img);
+  });
+};
+
+displayPost();
+
+
+// const herosIDontHave = arrayOfCharacters(3, 7, 9)
+//     herosIDontHave();
---
+++ main2.js
@@ -0,0 +1,10 @@
+// const getPosts = () => {
+//     fetch(type url here)
+//     .then(res =>) {
+//         if(!res.ok) {
+//             throw Error(res.statusText)
+//         } return res.json()
+//     })
+//     .then(post => arrayOfPosts = posts)
+//     .catch(err => console.log(`Error, ${err}`))
+// }
